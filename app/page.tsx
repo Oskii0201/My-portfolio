ddb3ed1bb85224df3a89ea636b2eb6916a5bd31d
Oskar <oskar0201@onet.pl>
2024-10-03 14:22:39 +0200

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Projects from "@/components/Projects";
+import Footer from "@/components/Footer";
+import TechStack from "@/components/TechStack";
+import Contact from "@/components/Contact";
+
 export default function Home() {
   return (
-    <main className="ml-24 flex min-h-screen flex-col pt-24 text-[#999999] md:ml-48 lg:ml-72">
-      Hello World!
+    <main className="ml-24 flex min-h-screen flex-col text-gray-300 md:ml-48 lg:ml-72">
+      <Hero />
+      <About />
+      <Projects />
+      <TechStack />
+      <Contact />
+      <Footer />
     </main>
   );
 }
