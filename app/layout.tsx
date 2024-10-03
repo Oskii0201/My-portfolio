0c185cb67186e51e7bf31630f68c04c1c84a0354
Oskar <oskar0201@onet.pl>
2024-10-03 14:16:51 +0200

    add scroll styles and behavior

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Sidebar } from "@/app/components/Sidebar/Sidebar";
+import { Sidebar } from "@/components/Sidebar/Sidebar";
 
 export const metadata: Metadata = {
   title: "Oskar Dybas Portfolio",
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scrollbar-global scroll-smooth">
       <body>
         <Sidebar />
         {children}
