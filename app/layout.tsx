7a3cf4168fac046c7a2572cb579e510c9e7c7973
Oskar <oskar0201@onet.pl>
2024-10-04 13:34:41 +0200

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,44 @@ import "./globals.css";
 import { Sidebar } from "@/components/Sidebar/Sidebar";
 
 export const metadata: Metadata = {
-  title: "Oskar Dybas Portfolio",
-  description: "Frontend developer portfolio",
+  title: "Oskar Dybas | Portfolio",
+  description: "Frontend developer portfolio showcasing projects and skills.",
+  keywords: [
+    "Frontend Developer",
+    "Portfolio",
+    "JavaScript",
+    "React",
+    "Next.js",
+    "Oskar Dybas",
+  ],
+  authors: [{ name: "Oskar Dybas", url: "https://www.oskardybas.com" }],
+  openGraph: {
+    title: "Oskar Dybas | Portfolio",
+    description: "Frontend developer portfolio showcasing projects and skills.",
+    url: "https://www.oskardybas.com",
+    siteName: "Oskar Dybas Portfolio",
+    images: [
+      {
+        url: "/images/preview.png",
+        width: 1200,
+        height: 630,
+        alt: "Oskar Dybas Portfolio Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Oskar Dybas Portfolio",
+    description: "Frontend developer portfolio showcasing projects and skills.",
+    images: ["/images/preview.png"],
+  },
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
