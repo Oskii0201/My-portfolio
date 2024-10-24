4f301de78185d3bb18f04e8fe4fc7e14c7e35563
Oskar <oskar0201@onet.pl>
2024-10-24 18:31:23 +0200

    style(footer): add missing space

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="mt-auto border-t border-[#272727] p-4 text-center text-sm md:text-base">
-      &copy;2024 All rights Reserved. Designed & developed by{" "}
+      &copy; 2024 All rights Reserved. Designed & developed by{" "}
       <Link
         href="https://linkedin.com/in/oskar-dybas"
         target="_blank"
